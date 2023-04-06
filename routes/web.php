055343c4b9f0a79555936d253d1f794cf28fb7e0
robinhermocilla <rhermocilla@gbox.adnu.edu.ph>
2023-04-06 23:26:31 +0800

    Created Migration and seeder

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/shop', [App\Http\Controllers\ProductController::class, 'index'])->name('shop');
